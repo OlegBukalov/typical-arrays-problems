dde2ca62655490d75aa02a2b5fe7f4bab052ab82
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-03-01 17:05:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    if (i+1 == array.length) {
+      return sum / (i+1);
+    }
+  }
 }
